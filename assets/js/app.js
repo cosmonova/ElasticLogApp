bc332cc8568bbd6329e7cb320ca11826dc00cecc
razvan.batin@gmail.com <razvan.batin@gmail.com>
2012-05-17 18:54:32 +0300

    gdf

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,9 @@ run(function () {
     // immediately invoked on first run
     var init = (function () {
         if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
+            //alert("No internet connection - we won't be able to show you any maps");
         } else {
-            alert("We can reach Google - get ready for some awesome maps!");
+            //alert("We can reach Google - get ready for some awesome maps!");
         }
     })();
     
